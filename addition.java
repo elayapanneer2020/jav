bb1d52ba66d87315e4b25061488b5c498b7baaf0
elayapanneer2020 <113533794+elayapanneer2020@users.noreply.github.com>
2022-09-15 21:18:58 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,16 @@
+package bharu;
+
+public class addition {
+
+	public static void main(String[] args) {
+		int num1,num2,ans;
+		num1=20;
+		num2=25;
+		ans=num1+num2;
+		System.out.println("sum="+ans);
+		System.out.println("the sum of"+"num1"+"and"+"num2"+ans);
+		// TODO Auto-generated method stub
+
+	}
+
+}
